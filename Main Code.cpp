9f0cbe7cefe43448e31fc2951c989ad507281ad0
leong308.eth <63528621+leong308@users.noreply.github.com>
2022-10-13 09:08:25 +0800

    Update Main Code.cpp

--- Main Code.cpp
+++ Main Code.cpp
@@ -1,15 +1,30 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
+#include <iostream> 
+#include <iomanip> 
+using namespace std; 
 
-int main()
-{
-	
-	
-	
-	
-	
-	
-	return 0;
-	
+int main() 
+{ 
+        char mlist;
+        unsigned int space;
+
+        // infinite loop while not Y or N
+        do{
+            cout << "Movie list?" << endl;
+            cin >> mlist;  
+
+            if(mlist != 'Y' && mlist != 'N')
+                cout << "Invalid input. Please try again.\n" << endl;
+        }  
+        while(mlist != 'Y' && mlist != 'N');
+          
+        switch(mlist)
+        {
+            case 'Y':   
+                        break;
+            case 'N':
+                        break;
+        }
+
+        return 0; 
+          
 }
